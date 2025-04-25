c37031bf9dd6b4bb9fe1bf982d5df837a7fe6b36
jsestoque <jerissa.mae.joelle.estoque@bankofmakati.com.ph>
2025-04-25 18:36:59 +0800

    update sidebar styling

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -8,9 +8,9 @@
 <body class="min-h-screen font-sans antialiased bg-base-200">
 
     {{-- NAVBAR mobile only --}}
-    <x-mary-nav sticky class="lg:hidden !bg-[#1B4298] text-white dark:!bg-red-800">
+    <x-mary-nav sticky class="lg:hidden !bg-[#1B4298] text-white">
         <x-slot:brand>
-            <img src="{{ asset('assets/images/bmi-banner.jpg') }}" class="w-70" alt="bmi-banner">
+            <img src="{{ asset('assets/images/bmi-banner.jpg') }}" class="h-10" alt="bmi-banner">
         </x-slot:brand>
         <x-slot:actions>
             <label for="main-drawer" class="mr-3 lg:hidden">
@@ -22,18 +22,18 @@
     {{-- MAIN --}}
     <x-mary-main full-width>
         {{-- SIDEBAR --}}
-        <x-slot:sidebar drawer="main-drawer" collapsible class="!bg-[#1B4298] lg:bg-inherit text-white dark:!bg-neutral-800">
+        <x-slot:sidebar drawer="main-drawer" collapsible class="!bg-[#1B4298] lg:bg-inherit text-white">
 
             {{-- BRAND --}}
             <a href="{{ route('dashboard') }}" class="mary-hideable">
                 <img src="{{ asset('assets/images/bmi-banner.jpg') }}" alt="bmi-banner">
             </a>
             <a href="{{ route('dashboard') }}" class="display-when-collapsed">
-                <img src="{{ asset('assets/images/bmi-logo.png') }}" class="h-15" alt="bmi-logo">
+                <img src="{{ asset('assets/images/bmi-logo.png') }}" alt="bmi-logo">
             </a>
 
             {{-- MENU --}}
-            <x-mary-menu activeBgColor="bg-[#F26531]" title="" activate-by-route>
+            <x-mary-menu activeBgColor="bg-[#F26531]" activate-by-route>
                 <x-mary-menu-item title="Home" icon="lucide.home" link="{{ route('dashboard') }}" route="dashboard"
                     class="hover:bg-[#F26531]" />
                 <x-mary-menu-item title="User Management" icon="lucide.users-2" link="{{ route('um.index') }}"
@@ -45,7 +45,7 @@
                     <x-mary-menu-item title="Account" icon="lucide.user-circle-2" link="{{ route('settings.profile') }}"
                         route="settings.*" />
                     <x-mary-menu-item title="Theme" icon="o-swatch" @click="$dispatch('mary-toggle-theme')" />
-                    <x-mary-theme-toggle darkTheme="business" class="hidden" />
+                    <x-mary-theme-toggle class="hidden" />
                     <x-mary-menu-item title="Logout" icon="lucide.log-out" link="/logout" />
                 </x-mary-menu-sub>
 
