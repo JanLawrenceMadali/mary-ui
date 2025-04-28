ac2316e598e03e9bb4e6448c067316d6270e3c08
jsestoque <jerissa.mae.joelle.estoque@bankofmakati.com.ph>
2025-04-28 18:25:41 +0800

    added styling on dark mode

--- resources/views/livewire/user-management/index-um.blade.php
+++ resources/views/livewire/user-management/index-um.blade.php
@@ -1,3 +1,3 @@
-<div class="p-4 bg-white border rounded-lg">
+<div class="p-4 bg-white border rounded-lg dark:bg-zinc-800 dark:border-zinc-700">
     <x-mary-table :headers="$headers" :rows="$users" />
 </div>
